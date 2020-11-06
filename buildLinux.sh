ceba8d44e13efde8d1b43c59836d64ca5cb8005e
geoliveira <38046853+geoliveira@users.noreply.github.com>
2020-11-06 01:11:12 -0300

    Update buildLinux.sh

--- buildLinux.sh
+++ buildLinux.sh
@@ -1,10 +1,5 @@
 #!/bin/bash 
-# Absolute path to this script
-SCRIPT=$(readlink -f "$0")
-# Absolute path this script is in
-SCRIPTPATH=$(dirname "$SCRIPT")
-cd $SCRIPTPATH
-clear
+
 mvn clean install
 jar xf lotus-zip/target/lotus-tool.zip
 mv lotus-tool/ lotus-zip/target/
